f32b850f74bcf794df11db1888cf80cd06982e77
Dominik Vozár <dominikvozr@gmail.com>
2022-09-27 21:13:02 +0200

    add: create singlton pattern

---
+++ main/src/main/java/org/example/DAO/DataSourceInterface.java
@@ -0,0 +1,5 @@
+package org.example.DAO;
+
+public interface DataSourceInterface {
+	void writeData(String orderName);
+}
--- main/src/main/java/org/example/Main.java
+++ main/src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
 	public static void main(String[] args) {
-
+		/*	priklad!!!
+			DataSource ds = DataSource.getInstance();
+			ds.writeData("Kebab");
+		*/
 	}
 }
