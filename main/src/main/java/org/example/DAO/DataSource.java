75278bebba133b1b58256cbd3926882dd34bb59a
Dominik Vozár <dominikvozr@gmail.com>
2022-09-28 00:35:41 +0200

    reflection, deserialization, cloning prevenion

--- main/src/main/java/org/example/DAO/DataSource.java
+++ main/src/main/java/org/example/DAO/DataSource.java
@@ -1,25 +1,14 @@
 package org.example.DAO;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
-public class DataSource implements DataSourceInterface {
+public class DataSource {
 	private static DataSource instance;
-	private DataSource() {}
+	private DataSource() {
+		if (instance != null) {
+			throw new RuntimeException();
+		}
+	}
 
 	public static DataSource getInstance() {
-		// The approach taken here is called double-checked locking (DCL). It
-		// exists to prevent race condition between multiple threads that may
-		// attempt to get singleton instance at the same time, creating separate
-		// instances as a result.
-		//
-		// It may seem that having the `result` variable here is completely
-		// pointless. There is, however, a very important caveat when
-		// implementing double-checked locking in Java, which is solved by
-		// introducing this local variable.
-		//
-		// You can read more info DCL issues in Java here:
-		// https://refactoring.guru/java-dcl-issue
 		DataSource result = instance;
 		if (result != null) {
 			return result;
@@ -32,16 +21,12 @@ public class DataSource implements DataSourceInterface {
 		}
 	}
 
+	protected Object readResolve() {
+		return instance;
+	}
+
 	@Override
-	public void writeData(String orderName) {
-		try {
-			FileWriter myWriter = new FileWriter("orders.txt");
-			myWriter.write("Order created: " + orderName + "\n");
-			myWriter.close();
-			System.out.println("Successfully wrote to the file.");
-		} catch (IOException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
+	protected Object clone() throws CloneNotSupportedException  {
+		throw new CloneNotSupportedException();
 	}
 }
--- main/src/main/java/org/example/Main.java
+++ main/src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import org.example.DAO.DataSource;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+
 public class Main {
 	public static void main(String[] args) {
-		/*	priklad!!!
-			DataSource ds = DataSource.getInstance();
-			ds.writeData("Kebab");
-		*/
+		DataSource instanceOne = DataSource.getInstance();
+		DataSource instanceTwo = (DataSource) instanceOne.clone();
+		System.out.println("hashCode of instance 1 - " + instanceOne.hashCode());
+		System.out.println("hashCode of instance 2 - " + instanceTwo.hashCode());
 	}
 }
