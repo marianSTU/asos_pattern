229af7a4beea1d15482cf27ee2610e21d6e66f13
simon <sajmoooonsk@gmail.com>
2022-09-27 09:27:44 +0200

    feat: add builder

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -3,7 +3,8 @@ package builder;
 public class Main {
 
     public static void main(String[] args){
-
+        Pizza pizza = Pizza.createPizza().setTomatoSauce(true).setHam(true).setCorn(true).setCheese(true).setSize("Small").build();
+        System.out.println(pizza.toString());
     }
 
 }
